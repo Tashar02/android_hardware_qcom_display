381c4666e227fc15000b10827acd5eb56b7f973c
Ken Huang <kenbshuang@google.com>
2022-06-20 12:55:32 +0300

    hwc2: set taskprofiles for hwc binder threads
    Add hwc threads that ever handled present display into SFMainPolicy
    task profiles group.
    
    Bug: 194533502
    Test: check effective uclamp.min
    
    Change-Id: I51723cc5e39bccb2c9df4c9e5b21ee1fbd55cf01
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- composer/hwc_session.cpp
+++ composer/hwc_session.cpp
@@ -47,6 +47,8 @@
 #include "hwc_debugger.h"
 #include "ipc_impl.h"
 
+#include <processgroup/processgroup.h>
+
 #define __CLASS__ "HWCSession"
 
 #define HWC_UEVENT_SWITCH_HDMI "change@/devices/virtual/switch/hdmi"
@@ -781,6 +783,14 @@ void HWCSession::PerformIdleStatusCallback(hwc2_display_t display) {
 int32_t HWCSession::PresentDisplay(hwc2_display_t display, shared_ptr<Fence> *out_retire_fence) {
   auto status = HWC2::Error::BadDisplay;
   DTRACE_SCOPED();
+
+  thread_local bool setTaskProfileDone = false;
+  if (setTaskProfileDone == false) {
+        if (!SetTaskProfiles(gettid(), {"SFMainPolicy"})) {
+          DLOGW("Failed to add `%d` into SFMainPolicy", gettid());
+      }
+      setTaskProfileDone = true;
+  }
 
   SCOPE_LOCK(system_locker_);
   if (display >= HWCCallbacks::kNumDisplays) {
